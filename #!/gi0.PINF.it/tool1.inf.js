98bf8e89674c609992ce473ac9b50ab001452a9f
Christoph Dorn <christoph@christophdorn.com>
2020-06-11 20:44:04 -0700

    misc

--- #!/gi0.PINF.it/tool1.inf.js
+++ #!/gi0.PINF.it/tool1.inf.js
@@ -47,6 +47,42 @@ exports['gi0.PINF.it/build/v0'] = async function (LIB, CLASSES) {
                 await api['#io.pinf/process~s3']({
                     result: result
                 });
+            } else
+            if (home.api === 'copy/v1') {
+
+                const sourcePath = await build.resolve(build.config.source);
+                const targetPath = target.path;
+
+                LIB.console.debug(`Copying file from`, sourcePath, `to`, targetPath);
+
+                result.inputPaths[sourcePath] = true;
+                result.outputPaths[targetPath] = true;
+
+                await LIB.FS.copy(sourcePath, targetPath);
+            } else
+            if (home.api === 'download/v1') {
+    
+                const url = build.config.url;
+                const expectedHash = build.config.hash || null;
+
+                LIB.console.debug(`Downloading file from url`, url, `with expected checksum`, expectedHash);
+
+                const content = await LIB.BENT('buffer')(url);
+                const actualHash = `sha1:${LIB.CRYPTO.createHash('sha1').update(content).digest('hex')}`;
+
+                if (!expectedHash) {
+                    throw `Downloaded url '${url}' with content hash '${actualHash}'! Add hash to config to proceeed!`;
+                }
+
+                if (actualHash !== expectedHash) {
+                    throw new Error(`Hash '${actualHash}' of downloaded content from '${url}' does not match expected hash '${expectedHash}'!`);
+                }
+
+//console.log("TODO: RECORD INPUT PATH:", result, build, target, instance, home, workspace);
+
+                result.outputPaths[target.path] = true;
+
+                await LIB.FS.outputFile(target.path, content);
             }
         }
     }
